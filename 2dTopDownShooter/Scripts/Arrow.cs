cc68fa8899eab5dfeb89f53c87098a836183144b
adrianignat <adrian.ignat@gmail.com>
2024-08-25 21:12:48 +0100

    Shoot in the direction you are facing

--- 2dTopDownShooter/Scripts/Arrow.cs
+++ 2dTopDownShooter/Scripts/Arrow.cs
@@ -1,4 +1,7 @@
+using dTopDownShooter.Scripts;
 using Godot;
+using Godot.Collections;
+using System;
 
 public partial class Arrow : RigidBody2D
 {
@@ -7,6 +10,8 @@ public partial class Arrow : RigidBody2D
 
 	[Export]
 	public float Speed { get; set; }
+
+	private Player _player;
 
 	public override void _Ready()
 	{
@@ -17,8 +22,78 @@ public partial class Arrow : RigidBody2D
 		MaxContactsReported = 1;
 
 		Scale = new Vector2(0.5f, 0.5f);
-		LinearVelocity = GetLocalMousePosition().Normalized() * Speed;
-		Rotation = GetLocalMousePosition().Angle();
+
+		_player = GetTree().Root.GetNode("main").GetNode<Player>("Player");
+
+		UpdateVelocity();
+		AdjustPosition();
+	}
+
+	private void AdjustPosition()
+	{
+		if (_player.Facing == Direction.W
+			&& _player.Moving != Direction.S
+			&& _player.Moving != Direction.SW)
+			Position = new Vector2(GlobalPosition.X - 50, GlobalPosition.Y);
+	}
+
+	private void UpdateRotation(Vector2 move_input)
+	{
+		var moving = DirectionHelper.GetMovingDirection(move_input);
+		if (moving == Direction.N) // Up
+			RotationDegrees = -90;
+		else if (moving == Direction.S) // Down
+			RotationDegrees = 90;
+		else if (moving == Direction.W) // Left
+			RotationDegrees = 180;
+		else if (moving == Direction.E) // Right
+			RotationDegrees = 0;
+		else if (moving == Direction.NW) // Up Left
+			RotationDegrees = -135;
+		else if (moving == Direction.NE) // Up Right
+			RotationDegrees = -45;
+		else if (moving == Direction.SW) // Down Left
+			RotationDegrees = 135;
+		else if (moving == Direction.SE) // Down Right
+			RotationDegrees = 45;
+	}
+
+	private void UpdateVelocity()
+	{
+		Vector2 move_input = Input.GetVector("left", "right", "up", "down");
+
+		if (move_input == Vector2.Zero)
+		{
+			switch (_player.Moving)
+			{
+				case Direction.N:
+					move_input = new Vector2(0, -1);
+					break;
+				case Direction.S:
+					move_input = new Vector2(0, 1);
+					break;
+				case Direction.W:
+					move_input = new Vector2(-1, 0);
+					break;
+				case Direction.E:
+					move_input = new Vector2(1, 0);
+					break;
+				case Direction.NW:
+					move_input = new Vector2(-1, -1);
+					break;
+				case Direction.NE:
+					move_input = new Vector2(1, -1);
+					break;
+				case Direction.SW:
+					move_input = new Vector2(-1, 1);
+					break;
+				case Direction.SE:
+					move_input = new Vector2(1, 1);
+					break;
+			}
+		}
+		LinearVelocity = move_input * Speed;
+		UpdateRotation(move_input);
 	}
 
 	private void OnCollision(Node body)
