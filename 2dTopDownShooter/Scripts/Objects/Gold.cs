047043722bf35627cb2b8cfa967f8f464a25b07d
adrianignat <adrian.ignat@gmail.com>
2025-10-26 21:49:11 +0000

    add magnet zone

--- 2dTopDownShooter/Scripts/Objects/Gold.cs
+++ 2dTopDownShooter/Scripts/Objects/Gold.cs
@@ -6,10 +6,49 @@ public partial class Gold : Area2D
 	[Export]
 	public ushort Amount { get; set; }
 
+	[Export]
+	public float MagnetSpeed { get; set; } = 300f;
+
+	private bool _isInMagnetZone = false;
+	private Player _player;
+
 	public override void _Ready()
 	{
 		var animation = GetNode<AnimatedSprite2D>("GoldAnimation");
 		animation.Play("spawn");
+
+		// Get player reference
+		_player = Game.Instance.Player;
+
+		// Connect area signals for magnet detection
+		AreaEntered += OnMagnetAreaEntered;
+		AreaExited += OnMagnetAreaExited;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		if (_isInMagnetZone && _player != null)
+		{
+			// Move toward player
+			Vector2 direction = (_player.GlobalPosition - GlobalPosition).Normalized();
+			GlobalPosition += direction * MagnetSpeed * (float)delta;
+		}
+	}
+
+	private void OnMagnetAreaEntered(Area2D area)
+	{
+		if (area.Name == "GoldMagnetArea")
+		{
+			_isInMagnetZone = true;
+		}
+	}
+
+	private void OnMagnetAreaExited(Area2D area)
+	{
+		if (area.Name == "GoldMagnetArea")
+		{
+			_isInMagnetZone = false;
+		}
 	}
 
 	private void OnCollision(Node body)
