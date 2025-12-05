cbc913489e63eab7f8ba697ef7b257d9db960f1c
adrianignat <adrian.ignat@gmail.com>
2025-12-05 19:09:59 +0000

    restore the initial enemy spawn rate

--- 2dTopDownShooter/Scripts/Characters/Enemies/EnemySpawner.cs
+++ 2dTopDownShooter/Scripts/Characters/Enemies/EnemySpawner.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 public partial class EnemySpawner : Spawner<Enemy>
 {
 	[Export]
-	public float SpawnIncreaseRate = 1.05f;
+	public float SpawnIncreaseRate = 1.1f;
 
 	/// <summary>
 	/// Time in seconds before each new enemy tier is introduced.
